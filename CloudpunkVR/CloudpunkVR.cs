b99d92b095df5cf7b136298b12634b2a61c1312a
Bernhard Eiling <bernhard.eiling@gmail.com>
2021-06-28 16:19:00 +0100

    Add project and empty mod

---
+++ CloudpunkVR/CloudpunkVR.cs
@@ -0,0 +1,21 @@
+﻿using MelonLoader;
+
+namespace CloudpunkVR
+{
+    public class CloudpunkVR : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            MelonLogger.Msg("OnUpdate");
+            //if (Input.GetKeyDown(KeyCode.T))
+            //{
+            //    MelonModLogger.Msg("You just pressed T");
+            //}
+        }
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            MelonLogger.Msg("OnSceneWasLoaded");
+            MelonLogger.Msg(sceneName);
+        }
+    }
+}
