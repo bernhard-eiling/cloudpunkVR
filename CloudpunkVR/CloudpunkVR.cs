b4a9474b31287e9c52e191f8d632ae83edbc666a
Bernhard Eiling <bernhard.eiling@gmail.com>
2021-06-28 21:32:42 +0100

    Create UI camera

--- CloudpunkVR/CloudpunkVR.cs
+++ CloudpunkVR/CloudpunkVR.cs
@@ -1,36 +1,20 @@
 ﻿using MelonLoader;
-using UnityEngine;
 
 namespace CloudpunkVR
 {
     public class CloudpunkVR : MelonMod
     {
-        private Camera guiCamera;
-        private Canvas guiCanvas;
+        private UI ui;
+
+        public override void OnApplicationStart()
+        {
+            ui = new UI();
+            ui.OnSetup();
+        }
 
         public override void OnUpdate()
         {
-            ensureGuiCanvas();
-        }
-
-        private bool ensureGuiCanvas()
-        {
-            if (guiCanvas != null)
-            {
-                return true;
-            }
-            foreach (var canvas in GameObject.FindObjectsOfType<Canvas>())
-            {
-                if (canvas.name == "Canvas-UI")
-                {
-                    guiCanvas = canvas;
-                    MelonLogger.Msg("Canvas UI found!");
-                    return true;
-                
-                }
-            }
-            MelonLogger.Msg("Canvas UI not found yet.");
-            return false;
+            ui.OnUpdate();
         }
     }
 }
---
+++ CloudpunkVR/UI.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using MelonLoader;
+
+namespace CloudpunkVR
+{
+    public class UI : MonoBehaviour
+    {
+        private Camera uiCamera;
+        private Canvas uiCanvas;
+        private RenderTexture uiTexture;
+
+        public void OnSetup()
+        {
+            createGuiCamera();
+        }
+
+        public void OnUpdate()
+        {
+            ensureGuiCanvas();
+        }
+
+        private void createGuiCamera()
+        {
+            uiTexture = new RenderTexture(new RenderTextureDescriptor(Screen.width, Screen.height));
+            GameObject uiCameraObject = new GameObject("uiCameraObject");
+            DontDestroyOnLoad(uiCameraObject);
+            uiCamera = uiCameraObject.AddComponent<Camera>();
+            uiCamera.orthographic = true;
+            uiCamera.targetTexture = uiTexture;
+            uiCamera.depth = 1;
+            uiCamera.useOcclusionCulling = false;
+            // This enables transparency on the GUI
+            // I tried "Depth" for the clear flags, but
+            // it had weird/unexpected results and Color
+            // just works...
+            uiCamera.clearFlags = CameraClearFlags.Color;
+            // Required to actually capture only the GUI layer
+            uiCamera.cullingMask = (1 << LayerMask.NameToLayer("UI"));
+            uiCamera.farClipPlane = 1.1f;
+            uiCamera.nearClipPlane = 0.9f;
+            uiCamera.enabled = true;
+            if (uiTexture == null)
+            {
+                MelonLogger.Msg("UI Texture not created.");
+            }
+            if (uiCamera == null)
+            {
+                MelonLogger.Msg("UI Camera not created.");
+            }
+            MelonLogger.Msg("createGuiCamera");
+        }
+
+        private bool ensureGuiCanvas()
+        {
+            if (uiCanvas != null)
+            {
+                return true;
+            }
+            foreach (var canvas in GameObject.FindObjectsOfType<Canvas>())
+            {
+                if (canvas.name == "Canvas-UI")
+                {
+                    MelonLogger.Msg("Canvas UI found!");
+                    uiCanvas = canvas;
+                    onGuiCanvasFound();
+                    return true;
+                }
+            }
+            MelonLogger.Msg("Canvas UI not found yet.");
+            return false;
+        }
+
+        private void onGuiCanvasFound()
+        {
+            MelonLogger.Msg(uiCanvas.ToString());
+            MelonLogger.Msg(uiCamera.ToString());  // this is null
+            MelonLogger.Msg(uiTexture.ToString());
+            uiCanvas.worldCamera = uiCamera;
+            uiCanvas.renderMode = RenderMode.WorldSpace;
+            uiCamera.gameObject.transform.position = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, -1);
+            uiCamera.orthographicSize = Screen.height * 0.5f;
+        }
+    }
+}
