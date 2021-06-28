817be4261c521756c631b006e187121787fa0261
Bernhard Eiling <bernhard.eiling@gmail.com>
2021-06-28 20:27:47 +0100

    Find the UI Canvas

--- CloudpunkVR/CloudpunkVR.cs
+++ CloudpunkVR/CloudpunkVR.cs
@@ -1,21 +1,36 @@
 ﻿using MelonLoader;
+using UnityEngine;
 
 namespace CloudpunkVR
 {
     public class CloudpunkVR : MelonMod
     {
+        private Camera guiCamera;
+        private Canvas guiCanvas;
+
         public override void OnUpdate()
         {
-            MelonLogger.Msg("OnUpdate");
-            //if (Input.GetKeyDown(KeyCode.T))
-            //{
-            //    MelonModLogger.Msg("You just pressed T");
-            //}
+            ensureGuiCanvas();
         }
-        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+
+        private bool ensureGuiCanvas()
         {
-            MelonLogger.Msg("OnSceneWasLoaded");
-            MelonLogger.Msg(sceneName);
+            if (guiCanvas != null)
+            {
+                return true;
+            }
+            foreach (var canvas in GameObject.FindObjectsOfType<Canvas>())
+            {
+                if (canvas.name == "Canvas-UI")
+                {
+                    guiCanvas = canvas;
+                    MelonLogger.Msg("Canvas UI found!");
+                    return true;
+                
+                }
+            }
+            MelonLogger.Msg("Canvas UI not found yet.");
+            return false;
         }
     }
 }
